01efcb4a9f7d712c32ef3f63263cb848705ff056
AndiOfTheLake <46411699+AndiOfTheLake@users.noreply.github.com>
2021-05-01 14:02:52 -0400

    first python script

---
+++ intro.py
@@ -0,0 +1,126 @@
+## As a calculator
+
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+# How much is your $100 worth after 7 years?
+print(100*(1.1 ** 7))
+
+## variables and types
+# Create a variable savings
+savings=100
+
+# Print out savings
+print(savings)
+
+# growth multiplier
+growth_multiplier = 1.1
+
+result = savings * growth_multiplier ** 7
+print(result)
+
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
+
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
+
+# exercise
+
+"I can add integers, like " + str(5) + " to strings."
+
+"I said " + ("Hey " * 2) + "Hey!"
+
+"The correct answer to this multiple choice exercise is answer number " + str(2)
+
+True + False
+
+## Python lists
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list areas
+areas = [hall, kit, liv, bed, bath]
+
+# Print areas
+print(areas)
+
+# Adapt list areas
+areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
+
+# Print areas
+print(areas)
+
+# possible ways to build lists
+[1, 3, 4, 2] 
+[[1, 2, 3], [4, 5, 7]] 
+[1 + 2, "a" * 5, 3]
+
+## Subsetting list
+# 0 indexing
+# list slicing 
+# [start(inclusive):end(exclusive)]
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Print out second element from areas
+print(areas[1])
+
+# Print out last element from areas
+print(areas[-1])
+
+# Print out the area of the living room
+print(areas[5])
+
+# Sum of kitchen and bedroom area: eat_sleep_area
+eat_sleep_area = areas[3] + areas[7]
+
+# Print the variable eat_sleep_area
+print(eat_sleep_area)
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Use slicing to create downstairs
+downstairs = areas[:6]
+
+# Use slicing to create upstairs
+upstairs = areas[6:10]
+
+# Print out downstairs and upstairs
+print(upstairs)
+print(downstairs)
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Alternative slicing to create downstairs
+downstairs = areas[:6]
+
+# Alternative slicing to create upstairs
+upstairs = areas[6:]

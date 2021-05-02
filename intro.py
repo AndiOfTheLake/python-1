19183915d8c9e720d5c785baa4ad59673895ae62
AndiOfTheLake <46411699+AndiOfTheLake@users.noreply.github.com>
2021-05-01 21:56:37 -0400

    command to install pkgs and update pip

--- intro.py
+++ intro.py
@@ -217,3 +217,68 @@ print(len(var1))
 # Convert var2 to an integer: out2
 out2 = int(var2)
 
+## Multiple arguments
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+full = first + second
+
+# Sort full in descending order: full_sorted
+full_sorted = sorted(full, reverse = True)
+
+# Print out full_sorted
+print(full_sorted)
+
+## Methods
+
+# String Methods
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place: place_up
+place_up = place.upper()
+
+# Print out place and place_up
+print(place)
+print(place_up)
+
+# Print out the number of o's in place
+print(place.count("o"))
+
+## List methods
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Print out the index of the element 20.0
+print(areas.index(20.0))
+
+# Print out how often 9.50 appears in areas
+print(areas.count(9.50))
+
+## List methods (2)
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Use append twice to add poolhouse and garage size
+areas.append(24.5)
+areas.append(15.45)
+
+
+# Print out areas
+print(areas)
+
+# Reverse the orders of the elements in areas
+areas.reverse()
+
+# Print out areas
+print(areas)
+
+## Packages
+# Install packages
+# In the terminal, type pip3 install package.name
+# update pip: python -m pip install --upgrade pip
+
+
+
